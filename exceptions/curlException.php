f90ddbfd64e7d2f48860f9c6c1a34c797d02213e
Dmitriy Emelyanov <emelyanov@infostars.ru>
2015-10-12 13:40:20 +0300

    Creating net-curl module

---
+++ exceptions/curlException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace mpcmf\system\exceptions;
+
+/**
+ * System curl exception class
+ *
+ * @author Gregory Ostrovsky <greevex@gmail.com>
+ * @date: 9/19/12 5:37 PM
+ */
+class curlException
+    extends \Exception
+{
+
+}
